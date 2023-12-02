e24c0a66ba6e58bdf564a103157f1e54d22cec91
korjun1993 <corojoon93@naver.com>
2023-12-02 16:05:39 +0900

    feat: WebClient URL 로깅

--- src/main/java/ai/viceversa/demo/config/OpenApiConfiguration.java
+++ src/main/java/ai/viceversa/demo/config/OpenApiConfiguration.java
@@ -6,12 +6,16 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import ai.viceversa.demo.api.OpenApiClient;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 public class OpenApiConfiguration {
 	private static final String DATA_API_URL = "https://apis.data.go.kr/B551011/PhotoGalleryService1?MobileOS=ETC&MobileApp=TEST&_type={dataType}&&serviceKey={secretKey}";
@@ -23,6 +27,7 @@ public class OpenApiConfiguration {
 		defaultUrlVariables.put("dataType", openApiProperties.dataType());
 
 		WebClient webClient = WebClient.builder()
+			.filter(logRequest())
 			.baseUrl(DATA_API_URL)
 			.defaultUriVariables(defaultUrlVariables)
 			.build();
@@ -33,5 +38,14 @@ public class OpenApiConfiguration {
 			.build();
 
 		return httpServiceProxyFactory.createClient(OpenApiClient.class);
+	}
+
+	@Bean
+	public ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+			log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+			clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+			return Mono.just(clientRequest);
+		});
 	}
 }
