72cc27f3c41da80df80acbbcf1a6ecb5a0fd9cd1
korjun1993 <corojoon93@naver.com>
2023-12-04 23:38:28 +0900

    feat: WebFlux 기반 조회

--- src/main/java/ai/viceversa/demo/service/PhotoFetchService.java
+++ src/main/java/ai/viceversa/demo/service/PhotoFetchService.java
@@ -1,14 +1,19 @@
 package ai.viceversa.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ai.viceversa.demo.api.OpenApiClient;
+import ai.viceversa.demo.dto.ItemDto;
 import ai.viceversa.demo.dto.PhotoFetchResponseDto;
 import ai.viceversa.demo.extension.ParsingUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -17,11 +22,23 @@ public class PhotoFetchService {
 	private final OpenApiClient openApiClient;
 	private final ObjectMapper objectMapper;
 
-	public PhotoFetchResponseDto fetchList(int numOfRows, int pageNo) {
-		return ParsingUtils.readValue(objectMapper, openApiClient.getPhotoList(numOfRows, pageNo));
+	public Flux<PhotoFetchResponseDto> fetchList(int numOfRows, int pageNo) {
+		return openApiClient.getPhotoList(numOfRows, pageNo).mapNotNull(
+			response -> ParsingUtils.readValue(objectMapper, response)
+		);
 	}
 
-	public PhotoFetchResponseDto fetchDetail(String title, int numOfRows, int pageNo) {
-		return ParsingUtils.readValue(objectMapper, openApiClient.getPhotoDetail(title, numOfRows, pageNo));
+	public Flux<PhotoFetchResponseDto> fetchDetail(String title, int numOfRows, int pageNo) {
+		return openApiClient.getPhotoDetail(title, numOfRows, pageNo)
+			.mapNotNull(response -> ParsingUtils.readValue(objectMapper, response));
+	}
+
+	public Mono<List<PhotoFetchResponseDto>> fetchDetailsJoinWithTitles(int fetchCountOfTitles, int PageNoOfTitles, int fetchCountOfDetails, int pageNoOfDetails) {
+		return fetchList(fetchCountOfTitles, PageNoOfTitles)
+			.map(PhotoFetchResponseDto::items)
+			.flatMap(Flux::fromIterable)
+			.map(ItemDto::galTitle)
+			.flatMap(title -> fetchDetail(title, fetchCountOfDetails, pageNoOfDetails))
+			.collectList();
 	}
 }
